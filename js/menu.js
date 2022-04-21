7016072da0a571b409ed3a1d42041b752d24afe0
joan kenedi mayo <talentojkm@gmail.com>
2022-04-21 16:16:52 -0500

    ultimoscambios

---
+++ js/menu.js
@@ -0,0 +1,13 @@
+const menu = document.getElementById('menu');
+const abrir = document.getElementById('abrir_menu');
+const link = document.querySelectorAll('.link');
+
+abrir.addEventListener('click', () => {
+    menu.classList.toggle('aparece_menu');
+    for(let i = 0; i < link.length; i++){
+        link[i].addEventListener('click', () =>{
+          menu.classList.remove('aparece_menu')
+      })
+    }
+})
+
